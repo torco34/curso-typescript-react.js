515cc1ca60676b2b7ace8899d99d3ceba3f976e8
torcoroma <torcoromaarias@gmail.com>
2023-09-02 11:26:08 -0500

    never fije

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { Header } from "./components/header/Header";
 // import { Pages } from "./components/curso-typescript/Pages";
 import { Outlet } from "./components/outlet/Outlet";
 import { Produecto } from "./components/outlet/Produecto";
-import { Practica } from "./components/practicas/Practica";
+import { PracticasPages } from "./components/practicas/PracticasPages";
 import { ReactHook } from "./components/hookReactForm/ReactHook";
 import { PagesApp } from "./components/MobX/PagesApp"
 import { Ejercicio } from "./components/useEfecto/Ejercicio.";
 
 import { Filter } from "./components/filter/pages/Filter";
+import { PagesReutilizar } from "./components/codigoReutilizable/PagesReutilizar";
+// import { ModalComponent } from "./components/codigoReutilizable/bootstrap-react/ModalComponent";
 function App() {
   return (
     <Router>
@@ -17,10 +19,12 @@ function App() {
         {/* ruta outlet */}
         <Route path="/outlet" element={<Produecto />}>
           <Route path="" element={<Outlet />}></Route>
-
         </Route>
         {/* ejemplo */}
-        <Route path="/ejem" element={<Practica />} />
+        <Route path="/practicas" element={<PracticasPages />} />
+        <Route path="/reutilizar" element={<PagesReutilizar />} >
+          {/* <Route path="/modal" element={<ModalComponent />} /> */}
+        </Route>
         <Route path="/hook" element={<ReactHook />} />
         <Route path="/mobx" element={<PagesApp />} />
         <Route path="/use" element={<Ejercicio />} />
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,25 +3,30 @@ import "./style.css"
 export const Header = () => {
   return (
     <div>
-      <nav className="bg-dark ">
-        <ul className="cont-nav">
-          <li>
+      <nav className="bg-dark p-4">
+        <ul className="cont-nav  ">
+          <li className="border border-info btn list-inline-item ">
             <Link to="/outlet">Outlet</Link>
           </li>
-          <li>
-            <Link to="/ejem">Ejemplo</Link>
+          <li className="border border-info  btn list-inline-item text-center ">
+            <Link to="/practicas">Practicas        </Link>
           </li>
-          <li>
+          <li className="border border-info btn list-inline-item ">
+            <Link to="/reutilizar">Código Reutiliza</Link>
+          </li>
+          <li className="border border-info btn list-inline-item ">
             <Link to="/hook">React hook form</Link>
           </li>
-          <li>
+          <li className="border border-info btn list-inline-item ">
             <Link to="/mobx">mobx</Link>
-
+          </li >
+          <li className="text-center btn list-inline-item ">
             <Link to="/use">useEffect</Link>
-          </li>
-          <li>
+          </li >
+          <li className="border border-info btn list-inline-item ">
             <Link to="/fil">filter</Link>
           </li>
+
         </ul>
       </nav>
     </div>
